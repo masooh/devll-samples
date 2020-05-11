b0f9657ba1d664f948205d5c1a4f73fb3dbb814f
Martin Hofmann-Sobik <melius-coder@gmx.de>
2020-05-11 22:23:43 +0200

    WIP: spring-profile

---
+++ spring/spring-profiles/src/test/java/org/devll/samples/spring/springprofiles/SpringProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devll.samples.spring.springprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
